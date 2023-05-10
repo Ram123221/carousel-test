bf93ef402f844262b887f333891eb53d33ddc765
R User L <ramgrg1024@gmail.com>
2023-05-10 22:27:54 +0545

    images.ts change again

--- src/components/images.ts
+++ src/components/images.ts
@@ -1,14 +1,14 @@
 const images:string[] =[
-    'src/images/item-1.jpg',
-    './src/images/item-2.jpeg',
-    './src/images/item-3.jpeg',
-    './src/images/item-4.jpeg',
-    './src/images/item-5.jpeg',
-    './src/images/item-6.jpeg',
-    './src/images/item-7.jpeg',
-    './src/images/item-8.jpeg',
-    './src/images/item-9.jpeg',
-    './src/images/item-10.jpeg',
+    './src/assets/images/item-1.jpg',
+    './src/assets/images/item-2.jpeg',
+    './src/assets/images/item-3.jpeg',
+    './src/assets/images/item-4.jpeg',
+    './src/assets/images/item-5.jpeg',
+    './src/assets/images/item-6.jpeg',
+    './src/assets/images/item-7.jpeg',
+    './src/assets/images/item-8.jpeg',
+    './src/assets/images/item-9.jpeg',
+    './src/assets/images/item-10.jpeg',
 ];
 
 export default images;
