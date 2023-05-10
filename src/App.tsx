be09f4bf74fe7a5bf6ef01444cf5ee0a2639edf5
R User L <ramgrg1024@gmail.com>
2023-05-10 19:57:14 +0545

    App.tsx small edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './App.scss';
 import images from './components/images';
 import {FaAngleDoubleRight, FaAngleDoubleLeft} from 'react-icons/fa';
