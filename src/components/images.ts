76e828c11dde6e17fff47f948b2c56d171781c79
R User L <ramgrg1024@gmail.com>
2023-05-10 22:32:42 +0545

    ... version2.0

--- src/components/images.ts
+++ src/components/images.ts
@@ -1,6 +1,6 @@
 const images:string[] =[
-    './src/assets/images/item-1.jpg',
-    './src/assets/images/item-2.jpeg',
+    './images/item-1.jpg',
+    './src/images/item-2.jpeg',
     './src/assets/images/item-3.jpeg',
     './src/assets/images/item-4.jpeg',
     './src/assets/images/item-5.jpeg',
