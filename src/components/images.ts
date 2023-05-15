fe2871f308972c463c1c808173c5b58950115ffa
R User L <ramgrg1024@gmail.com>
2023-05-15 12:01:33 +0545

    images.ts changed againgit add src/components/images.ts!

--- src/components/images.ts
+++ src/components/images.ts
@@ -1,6 +1,6 @@
 const images:string[] =[
-    'https://github.com/Ram123221/carousel-react/src/images/item-1.jpg',
-    './src/images/item-2.jpeg',
+    './src/assets/images/item-1.jpg',
+    './src/assets/images/item-2.jpeg',
     './src/assets/images/item-3.jpeg',
     './src/assets/images/item-4.jpeg',
     './src/assets/images/item-5.jpeg',
