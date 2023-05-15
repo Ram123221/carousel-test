b95e35196ca0b2ae2d3236c84fd52025b607ea3a
R User L <ramgrg1024@gmail.com>
2023-05-15 13:17:08 +0545

    last version can be uploaded to vercel

--- src/components/images.ts
+++ src/components/images.ts
@@ -1,14 +1,14 @@
 const images:string[] =[
-    './src/assets/images/item-1.jpg',
-    './src/assets/images/item-2.jpeg',
-    './src/assets/images/item-3.jpeg',
-    './src/assets/images/item-4.jpeg',
-    './src/assets/images/item-5.jpeg',
-    './src/assets/images/item-6.jpeg',
-    './src/assets/images/item-7.jpeg',
-    './src/assets/images/item-8.jpeg',
-    './src/assets/images/item-9.jpeg',
-    './src/assets/images/item-10.jpeg',
+    './public/assets/images/item-1.jpg',
+    './public/assets/images/item-2.jpeg',
+    './public/assets/images/item-3.jpeg',
+    './public/assets/images/item-4.jpeg',
+    './public/assets/images/item-5.jpeg',
+    './public/assets/images/item-6.jpeg',
+    './public/assets/images/item-7.jpeg',
+    './public/assets/images/item-8.jpeg',
+    './public/assets/images/item-9.jpeg',
+    './public/assets/images/item-10.jpeg',
 ];
 
 export default images;
